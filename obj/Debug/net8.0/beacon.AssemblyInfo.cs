891e8f3698bf0e0661211dc9456e3bb05fda261a
JancoNieuwoudt <149944183+JancoNieuwoudt@users.noreply.github.com>
2024-08-19 13:09:01 +0200

    initial pull

--- obj/Debug/net8.0/beacon.AssemblyInfo.cs
+++ obj/Debug/net8.0/beacon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beacon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7c2e3213376b18470881eb47c0a3a5ac7d7c7d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c957b1237033cf44eb3a97690f599ff27576ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("beacon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beacon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
