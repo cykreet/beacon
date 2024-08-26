37d38ebc89a6e9ac32f021b5fdf64b38b6c9cc85
cykreet <cykreet@gmail.com>
2024-08-26 10:23:26 +0200

    feat: size test compression ratios

--- WorkspaceTests/SizeWorkspaceTest.cs
+++ WorkspaceTests/SizeWorkspaceTest.cs
@@ -1,37 +1,50 @@
-﻿using Beacon.WorkspaceTests;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using System.Web;
 
 namespace Beacon.WorkspaceTests;
 
 [TestName("Size")]
-[TestDescription("Check if any files in the workspace is to large")]
+[TestDescription(
+  "Check if any files in the workspace exceed the size limit, compares individual compression ratios of files.")]
 internal class SizeWorkspaceTest : WorkspaceTest {
-  private const long maxLeng = 64 * 1024 * 1024;
+  private const double maxWorkspaceSizeBytes = 64 * 1024 * 1024;
+  private const double defaultCompressionRatio = 0.7;
+
+  private readonly Dictionary<string, double> mimeTypeCompressionRatios = new() {
+    { "text/*", 0.3 },
+    { "image/*", 0.95 },
+    { "audio/*", 0.95 },
+    { "video/*", 0.99 },
+    { "application/zip", 1.0 },
+    { "application/vnd.openxmlformats-officedocument.*", 0.5 },
+    { "application/octet-stream", 0.95 }
+  };
 
   protected override bool validate(TestContext context) {
-    // todo: check individual file sizes and warn about largest file sizes
-    // ExtensionWorkspace test does something similar.
-    bool allFilesValid = true;
-    foreach (ZipArchiveEntry entry in context.zipArchive.entries) 
-    {
-       
-      
-        if(context.zipArchive.entries.Length > maxLeng) 
-        {
-          this.addWarning("The file is to large");
-          allFilesValid = false;
-        }
-        //context.zipArchive.entries.All(entry => entry.Length < maxLeng);
-      
-    }
-    return allFilesValid;
-    
+    var workspaceSize = context.zipArchive.entries.Aggregate(0f, (acc, entry) => {
+      var mimeType = MimeMapping.GetMimeMapping(entry.Name);
+      Sentry.debug($"file mime: {mimeType}");
+      var compressionRatio = this.getCompressionRatio(mimeType);
+      Sentry.debug($"compression ratio: {compressionRatio}");
+      var estimatedCompressedSize = entry.Length * compressionRatio;
+      const double threshold = maxWorkspaceSizeBytes / 2;
+      var sizeMegaBytes = entry.Length / 1e+6;
+      if (estimatedCompressedSize > threshold)
+        this.addWarning($"File {entry.Name} is unusually large at length {sizeMegaBytes}MB.");
+      return acc += entry.Length;
+    });
+
+    return workspaceSize <= maxWorkspaceSizeBytes;
+  }
+
+  /*
+   * gets compression ratios from the dictionary with added wildcard support.
+   */
+  private double getCompressionRatio(string mimeType) {
+    var match = this.mimeTypeCompressionRatios.FirstOrDefault(ratio =>
+      ratio.Key == mimeType || (ratio.Key.EndsWith("/*") && mimeType.StartsWith(ratio.Key.TrimEnd('*'))) ||
+      (ratio.Key.EndsWith("*") && mimeType.EndsWith(ratio.Key.TrimEnd('*'))));
+    return match.Key != null ? match.Value : defaultCompressionRatio;
   }
 }
